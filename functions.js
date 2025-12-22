aade136175ba7f3425eab1e36edd6297e9eb7e98
Unicorn X <dudebrothers2@gmail.com>
2025-12-22 22:32:49 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,1 +1,15 @@
+import axios from "axios";
+
+const API = `https://api.telegram.org/bot${process.env.BOT_TOKEN}`;
+
+export async function sendMessage(chatId, text) {
+  return axios.post(`${API}/sendMessage`, {
+    chat_id: chatId,
+    text
+  });
+}
+
+export function isAdmin(userId) {
+  return process.env.ADMIN_IDS.split(",").includes(String(userId));
+}
 
