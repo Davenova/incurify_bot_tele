77cbe3f2771c32d41e65daa09f3d529584ffdb17
Unicorn X <dudebrothers2@gmail.com>
2025-12-23 01:54:56 +0530

    Update main.js

--- api/main.js
+++ api/main.js
@@ -1,26 +1,15 @@
-import { handleMessage, handleCallback } from "../commands.js";
+import { handleCommand } from "../commands.js";
 
 export default async function handler(req, res) {
-  try {
-    if (req.method !== "POST") {
-      return res.status(200).send("OK");
-    }
-
-    const update = req.body;
-
-    // Message handler
-    if (update.message) {
-      await handleMessage(update.message);
-    }
-
-    // Inline button handler
-    if (update.callback_query) {
-      await handleCallback(update.callback_query);
-    }
-
+  if (req.method !== "POST") {
     return res.status(200).send("OK");
-  } catch (err) {
-    console.error("Webhook error:", err);
-    return res.status(200).send("ERROR");
   }
+
+  const update = req.body;
+
+  if (update.message?.text) {
+    await handleCommand(update.message);
+  }
+
+  return res.status(200).send("OK");
 }
