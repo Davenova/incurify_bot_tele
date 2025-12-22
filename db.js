3b8bd10804fde489c6fd3a35af4f9cf2991f7cc0
Unicorn X <dudebrothers2@gmail.com>
2025-12-23 00:56:12 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,41 +1,36 @@
-import { MongoClient } from "mongodb";
-
-const client = new MongoClient(process.env.MONGO_URI);
-let db;
-
-async function connect() {
-  if (!db) {
-    await client.connect();
-    db = client.db("telegramBot");
-  }
-  return db;
-}
-
-export async function saveUserBase(user) {
-  const database = await connect();
-  await database.collection("users").updateOne(
-    { telegramId: user.telegramId },
-    { $setOnInsert: user },
-    { upsert: true }
+export async function updateUser(telegramId, data) {
+  const db = await connectDB();
+  return db.collection("users").updateOne(
+    { telegramId },
+    { $set: data }
   );
 }
 
-export async function updateUser(query, data) {
-  const database = await connect();
-  return database.collection("users").updateOne(query, { $set: data });
+export async function getUserByAny(identifier) {
+  const db = await connectDB();
+
+  if (String(identifier).startsWith("@")) {
+    return db.collection("users").findOne({ username: identifier.slice(1) });
+  }
+
+  return db.collection("users").findOne({
+    $or: [
+      { telegramId: Number(identifier) },
+      { username: identifier }
+    ]
+  });
 }
 
-export async function getUser(query) {
-  const database = await connect();
-  return database.collection("users").findOne(query);
+export async function deleteUser(identifier) {
+  const db = await connectDB();
+  const user = await getUserByAny(identifier);
+  if (!user) return null;
+
+  await db.collection("users").deleteOne({ telegramId: user.telegramId });
+  return user;
 }
 
 export async function getAllUsers() {
-  const database = await connect();
-  return database.collection("users").find().toArray();
-}
-
-export async function deleteUser(query) {
-  const database = await connect();
-  return database.collection("users").deleteOne(query);
+  const db = await connectDB();
+  return db.collection("users").find().toArray();
 }
