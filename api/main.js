55f4e2d877608bf2ce820e0736a56f3cfee7e902
Unicorn X <dudebrothers2@gmail.com>
2025-12-22 22:33:10 +0530

    Update main.js

--- api/main.js
+++ api/main.js
@@ -1,1 +1,16 @@
+import { handleCommand } from "../commands.js";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(200).send("OK");
+  }
+
+  const update = req.body;
+
+  if (update.message?.text) {
+    await handleCommand(update.message);
+  }
+
+  res.status(200).send("OK");
+}
 
