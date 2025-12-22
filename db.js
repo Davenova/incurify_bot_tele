6229fb918e31d83ea1229a7341e9e6089c54bdb3
Unicorn X <dudebrothers2@gmail.com>
2025-12-23 01:34:39 +0530

    Update db.js

--- db.js
+++ db.js
@@ -84,29 +84,25 @@ export async function updateUser(telegramId, data) {
 
 export async function getUserByAny(identifier) {
   const db = await connectDB();
-
-  if (String(identifier).startsWith("@")) {
-    return db.collection("users").findOne({ username: identifier.slice(1) });
-  }
-
   return db.collection("users").findOne({
     $or: [
       { telegramId: Number(identifier) },
-      { username: identifier }
+      { username: identifier.replace("@", "") }
     ]
   });
-}
-
-export async function deleteUser(identifier) {
-  const db = await connectDB();
-  const user = await getUserByAny(identifier);
-  if (!user) return null;
-
-  await db.collection("users").deleteOne({ telegramId: user.telegramId });
-  return user;
 }
 
 export async function getAllUsers() {
   const db = await connectDB();
   return db.collection("users").find().toArray();
+}
+
+export async function deleteUser(identifier) {
+  const db = await connectDB();
+  return db.collection("users").deleteOne({
+    $or: [
+      { telegramId: Number(identifier) },
+      { username: identifier.replace("@", "") }
+    ]
+  });
 }
