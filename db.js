7120b57489990b5f4a1468dc7d539889cda98a63
Unicorn X <dudebrothers2@gmail.com>
2025-12-23 00:19:38 +0530

    Update db.js

--- db.js
+++ db.js
@@ -73,3 +73,40 @@ export async function clearState(telegramId) {
   const database = await connectDB();
   await database.collection("states").deleteOne({ telegramId });
 }
+
+export async function updateUser(telegramId, data) {
+  const db = await connectDB();
+  return db.collection("users").updateOne(
+    { telegramId },
+    { $set: data }
+  );
+}
+
+export async function getUserByAny(identifier) {
+  const db = await connectDB();
+
+  if (String(identifier).startsWith("@")) {
+    return db.collection("users").findOne({ username: identifier.slice(1) });
+  }
+
+  return db.collection("users").findOne({
+    $or: [
+      { telegramId: Number(identifier) },
+      { username: identifier }
+    ]
+  });
+}
+
+export async function deleteUser(identifier) {
+  const db = await connectDB();
+  const user = await getUserByAny(identifier);
+  if (!user) return null;
+
+  await db.collection("users").deleteOne({ telegramId: user.telegramId });
+  return user;
+}
+
+export async function getAllUsers() {
+  const db = await connectDB();
+  return db.collection("users").find().toArray();
+}
