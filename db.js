f3a1c48f5626b5db10b7240c06c4dda2d35ff64a
Unicorn X <dudebrothers2@gmail.com>
2025-12-22 22:32:36 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,41 @@
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGO_URI);
+let db;
+
+export async function connectDB() {
+  if (!db) {
+    await client.connect();
+    db = client.db("telegramBot");
+  }
+  return db;
+}
+
+export async function saveUser(userData) {
+  const database = await connectDB();
+  const users = database.collection("users");
+
+  await users.updateOne(
+    { telegramId: userData.telegramId },
+    { $setOnInsert: userData },
+    { upsert: true }
+  );
+}
+
+export async function toggleFeature(name) {
+  const database = await connectDB();
+  const features = database.collection("features");
+
+  const feature = await features.findOne({ name });
+
+  if (!feature) {
+    await features.insertOne({ name, enabled: false });
+    return false;
+  }
+
+  const newState = !feature.enabled;
+  await features.updateOne({ name }, { $set: { enabled: newState } });
+
+  return newState;
+}
 
