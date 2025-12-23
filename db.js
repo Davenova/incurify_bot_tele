523b52be34ef382a6a09865fd6eca1df5606c098
Unicorn X <dudebrothers2@gmail.com>
2025-12-24 03:06:57 +0530

    Update db.js

--- db.js
+++ db.js
@@ -11,6 +11,7 @@ export async function connectDB() {
   return db;
 }
 
+/* USER MANAGEMENT */
 export async function saveUserBasic(user) {
   const database = await connectDB();
   await database.collection("users").updateOne(
@@ -31,7 +32,6 @@ export async function updateUser(telegramId, data) {
 export async function getUser(query) {
   const database = await connectDB();
   
-  // Try to parse as number for telegramId lookup
   const numQuery = Number(query);
   
   return database.collection("users").findOne({
@@ -54,7 +54,6 @@ export async function getAllUsers() {
 export async function deleteUser(query) {
   const database = await connectDB();
   
-  // Try to parse as number
   const numQuery = Number(query);
   
   return database.collection("users").deleteOne({
@@ -83,4 +82,67 @@ export async function getState(telegramId) {
 export async function clearState(telegramId) {
   const database = await connectDB();
   await database.collection("states").deleteOne({ telegramId });
+}
+
+/* SETTINGS */
+export async function getSettings() {
+  const database = await connectDB();
+  return database.collection("settings").findOne({ _id: "main" });
+}
+
+export async function updateSettings(data) {
+  const database = await connectDB();
+  await database.collection("settings").updateOne(
+    { _id: "main" },
+    { $set: { ...data, updatedAt: new Date() } },
+    { upsert: true }
+  );
+}
+
+/* TICKET SYSTEM */
+export async function createTicket(ticket) {
+  const database = await connectDB();
+  await database.collection("tickets").insertOne(ticket);
+}
+
+export async function getTicket(ticketId) {
+  const database = await connectDB();
+  return database.collection("tickets").findOne({ ticketId: Number(ticketId) });
+}
+
+export async function getAllTickets() {
+  const database = await connectDB();
+  return database.collection("tickets")
+    .find({})
+    .sort({ createdAt: -1 })
+    .toArray();
+}
+
+export async function getOpenTickets() {
+  const database = await connectDB();
+  return database.collection("tickets")
+    .find({ status: { $ne: "closed" } })
+    .sort({ createdAt: -1 })
+    .toArray();
+}
+
+export async function getTicketsByUser(userId) {
+  const database = await connectDB();
+  return database.collection("tickets")
+    .find({ userId })
+    .sort({ createdAt: -1 })
+    .toArray();
+}
+
+export async function updateTicket(ticketId, data) {
+  const database = await connectDB();
+  await database.collection("tickets").updateOne(
+    { ticketId: Number(ticketId) },
+    { $set: { ...data, updatedAt: new Date() } }
+  );
+}
+
+export async function deleteTicket(ticketId) {
+  const database = await connectDB();
+  await database.collection("tickets").deleteOne({ ticketId: Number(ticketId) });
 }
